ca284eccaf97bba32910fbd7aabd3a01fad6e080
okiverny <oleg.kivernyk@gmail.com>
2024-12-18 23:27:00 +0100

    optimize df functions

---
+++ utils.py
@@ -0,0 +1,74 @@
+import polars as pl
+import numpy as np
+
+def reduce_memory_usage_pl(df: pl.DataFrame) -> pl.DataFrame:
+    """ Reduce memory usage by polars dataframe {df} with name {name} by changing its data types."""
+    
+    print(f"Memory usage of dataframe is {round(df.estimated_size('mb'), 2)} MB")
+    Numeric_Int_types = [pl.Int8,pl.Int16,pl.Int32,pl.Int64]
+    Numeric_Float_types = [pl.Float32,pl.Float64]
+    for col in df.columns:
+        if col in ['date_id', 'time_id', 'symbol_id', 'weight']:
+            continue
+
+        col_type = df[col].dtype
+        c_min = df[col].min()
+        c_max = df[col].max()
+        # Ignore columns with all nulls
+        if not c_min: continue
+
+        # Casting
+        if col_type in Numeric_Int_types:
+            if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                df = df.with_columns(df[col].cast(pl.Int8))
+            elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                df = df.with_columns(df[col].cast(pl.Int16))
+            elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                df = df.with_columns(df[col].cast(pl.Int32))
+            elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                df = df.with_columns(df[col].cast(pl.Int64))
+        elif col_type in Numeric_Float_types:
+            if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                df = df.with_columns(df[col].cast(pl.Float32))
+            else:
+                pass
+        #elif col_type == pl.Utf8:
+        #    df = df.with_columns(df[col].cast(pl.Categorical))
+        else:
+            pass
+
+    print(f"Memory usage after optimization is: {round(df.estimated_size('mb'), 2)} MB")
+    return df
+
+def reduce_mem_usage(df, float16_as32=True):
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+        if col_type != object and str(col_type)!='category':
+            c_min,c_max = df[col].min(),df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    if float16_as32:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float16)  
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+
+    return df
